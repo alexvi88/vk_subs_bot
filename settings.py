2b07485933a570c8fb776010721b2a6c4bd6f786
alexvi88 <37776143+alexvi88@users.noreply.github.com>
2018-05-19 01:32:08 +0300

    19/05/2018

---
+++ settings.py
@@ -0,0 +1,7 @@
+access_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+confirmation_key = 'xxxxxxxx'
+secret_key = 'xxxxxxxxxxxxxxxxxxx'
+bot_group_id = 'xxxxxxxxx'
+bot_group_link = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+service_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+vk_api_version = 5.75
