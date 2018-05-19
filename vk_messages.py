cfe3ce7df3a411454749222fa52c3d465fe01596
alexvi88 <37776143+alexvi88@users.noreply.github.com>
2018-05-20 01:26:07 +0300

    20/05/2018

--- vk_messages.py
+++ vk_messages.py
@@ -3,7 +3,7 @@ import settings
 
 
 def send_message_to_user(user_id, message):
-    session = vk.Session()
+    session = vk.Session(access_token=settings.access_key)
     api = vk.API(session, v=settings.vk_api_version)
     api.messages.send(access_token=settings.access_key, user_id=user_id, message=message)
 
@@ -12,4 +12,3 @@ def send_post_to_user(user_id, post_info):
     session = vk.Session(access_token=settings.access_key)
     vk_api = vk.API(session, v=settings.vk_api_version)
     vk_api.messages.send(user_id=user_id, attachment=post_info, peer_id=settings.bot_group_id)
-    
