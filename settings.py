d2e6d14babd9e7b22b6c8bfed9b337c21a708c92
alexvi88 <37776143+alexvi88@users.noreply.github.com>
2018-05-21 01:19:18 +0300

    21/05/2018

--- settings.py
+++ settings.py
@@ -5,3 +5,4 @@ bot_group_id = 'xxxxxxxxx'
 bot_group_link = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 service_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 vk_api_version = 5.75
+directory_with_users = "/home/alexander888/mysite/users"
